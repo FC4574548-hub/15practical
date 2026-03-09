65c3fe6eb67e30acc172a7874ef8e2edeb48c322
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-09 15:29:39 +0200

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.*;
 
 public class Anagrams {
@@ -19,11 +17,11 @@ public class Anagrams {
                     " %s" + args[1]);
             return;
         }
-        String inputFile = args[0];
-        String outputFile = args[1];
+        String inputFile = "joyce1922_ulysses.text";
+        String outputFile = "Output";
         System.out.println("Data file: " + inputFile);
 
-        
+
         BufferedReader reader = new BufferedReader((new FileReader(inputFile)));
 
         HashMap<String, Integer> D = new HashMap<>();
@@ -39,22 +37,22 @@ public class Anagrams {
 
                 if (w.length() == 0)
                     continue;
-                    
+
                 if (D.containsKey(w))
                     D.put(w, D.get(w) + 1);
                 else D.put(w, 1);
             }
 
         }
-            
+
         reader.close();
 
         HashMap<String, ArrayList<String>> A = new HashMap<>();
-        
+
         for (String w : D.keySet()){
-            
+
             String a = signature(w);
-            
+
             if (!A.containsKey(a)) {
                 A.put(a, new ArrayList<String>());
             }
@@ -63,28 +61,55 @@ public class Anagrams {
 
 
         BufferedReader qreader = new BufferedReader((new FileReader(outputFile)));
-        
+
         while ((line = qreader.readLine()) != null && !line.equals("")){
             String word = line.trim();
-            
+
             System.out.println("<" + word + ">");
-            
+
             String tryanagram = signature((word));
-            
+
             if(A.containsKey(tryanagram)){
                 System.out.println(word + " " + A.get(tryanagram));
 
             }        }
-        
+
         qreader.close();
+
+        PrintWriter writer = new PrintWriter(new FileWriter("anagrams.txt"));
         
-        
-        
+        for (String key : A.keySet()){
+            ArrayList<String> list = A.get(key);
+            
+            if (list.size() > 1) {
+                String anagramList = String.join("", list);
+                writer.println((anagramList));
+                
+                for (int i = 0;i < list.size()- 1; i++){
+                    String[] parts = anagramList.split("");
+                    anagramList = "";
+                    
+                    for (int j = 0; j <parts.length; j++)
+                        anagramList += parts[j] + "";
+                    anagramList +=  parts[0];
+                    
+                    writer.println(anagramList);
+                    
+                }
+                writer.close();
+                
+                
+                
+                
+            }
+        }
+
+
+
     }
 
 
 
 }
-
 
 
