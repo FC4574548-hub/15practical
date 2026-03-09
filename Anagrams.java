2f883d618d8b669c372fafd2bfd410a8c01d9bef
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-09 14:40:53 +0200

    Update Anagrams.java

--- Anagrams.java
+++ Anagrams.java
@@ -9,5 +9,24 @@ public class Anagrams {
     return new String(chars);
   }
 
+  public static void main(String[] args){
+
+    if (arge.length != 2) {
+      System.out.println("USage: ./anagrans inputfile. \n You gave: \n %s" % args[1]);
+      return;
+    }
+    String inputFile = args[0];
+    String outputFile = args[1];
+    System.out.println("Data file: " + inputFile);
+
+    HashMap<String, Integer> D = new HashMap<>();
+    String line;
+
+    try {
+    
+
+    
+  }
+
   
 }
