34f6927c3d2d20e514f0bccfc88963621c618cd1
FC4574548-hub <4574548@myuwc.ac.za>
2026-03-09 14:28:11 +0200

    Just added the signature function

--- Anagrams.java
+++ Anagrams.java
@@ -1,1 +1,13 @@
+import java.io.*;
+import java.util.*;
 
+public class Anagrams {
+
+  public static String signature(String word){
+    char[] chars = word.toCharArray();
+    Arrays.sort(chars);
+    return new String(chars);
+  }
+
+  
+}
